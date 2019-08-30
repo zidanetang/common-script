f25da3452177a337ad0d74dbb6a140f2b648f3a7
Melo.tang <tjyao520@126.com>
2019-08-30 17:57:16 +0800

    fix

--- mongoDB/install_mongo.sh
+++ mongoDB/install_mongo.sh
@@ -134,7 +134,7 @@ if [ $mode = replication ]; then
 	PIDFILE=/var/run/mongo$port.pid
 	DAEMON=/usr/bin/mongod
 	DAEMON_OPTS="--config /etc/mongod/mongo$port/mongod.conf"
-        start-stop-daemon --background --start --quieti --pidfile $PIDFILE \
+        start-stop-daemon --background --start --quiet --pidfile $PIDFILE \
                         --make-pidfile --chuid mongodb:mongodb \
                         --exec $DAEMON -- $DAEMON_OPTS
     done
