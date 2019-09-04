42fadb1908aec4769a2e92c8470853fede5d9622
tjyao520 <tjyao520@126.com>
2019-09-04 16:28:05 +0800

    Update

--- mongoDB/omogo/handler/handler.go
+++ mongoDB/omogo/handler/handler.go
@@ -8,7 +8,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
+	//"go.mongodb.org/mongo-driver/mongo/readpref"
 	"go.mongodb.org/mongo-driver/x/mongo/driver/uuid"
 	cli "gopkg.in/urfave/cli.v2"
 	"math/rand"
